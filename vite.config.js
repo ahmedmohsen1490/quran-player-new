dc67a4ab1dac0a240147fe2947a403908ba18182
Ahmed Mohsen <ahmedmohsen1490@gmail.com>
2025-10-09 09:21:00 +0300

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/quran-player-new/",
   plugins: [react()],
   build: {
     outDir: 'dist',
